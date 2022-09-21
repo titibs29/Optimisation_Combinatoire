eb29fd1b1f717f921f803d6e9b94f10b56b23504
Veysf <florent.veys@std.heh.be>
2022-09-21 23:20:02 +0200

    Fonction Calcul du cout d'une solution

--- Grasp/Grasp/Grasp.cpp
+++ Grasp/Grasp/Grasp.cpp
@@ -7,13 +7,6 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
-struct Solution {
-    vector<int> agencement;
-    vector<int> nbImpression;
-    int coutTotal;
-};
 /*
 struct InputData {
     int nbCouverture;
@@ -32,14 +25,21 @@ struct OutputData
 };
 */
 
-void lecture(vector<float>* input, int* nbDataset) {
 
-    ifstream fichier("Dataset-Dev/I" + to_string(*nbDataset) + ".in");  //Ouverture d'un fichier en lecture
+struct Solution {
+    std::vector<int> agencement;
+    std::vector<int> nbImpression;
+    float coutTotal;
+};
+
+void lecture(std::vector<float>* input, int* nbDataset) {
+
+    std::ifstream fichier("Dataset-Dev/I" + std::to_string(*nbDataset) + ".in");  //Ouverture d'un fichier en lecture
 
     if (fichier)
     {
         //Tout est prêt pour la lecture.
-        string donnee;
+        std::string donnee;
 
         while(fichier >> donnee){
             input->push_back(stof(donnee));
@@ -48,53 +48,72 @@ void lecture(vector<float>* input, int* nbDataset) {
     }
     else
     {
-        cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+        std::cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << std::endl;
     }
 }
 
 void ecriture(float* nbEmplacement, Solution* best, int* nbDataset) {
-    ofstream fichier("output/O" + to_string(*nbDataset) + ".out");
+    std::ofstream fichier("output/O" + std::to_string(*nbDataset) + ".out");
     if (fichier)
     {
         int nbPlaques = best->agencement.size() % ((int)*nbEmplacement-1);
-        fichier << nbPlaques << endl;
-        for(int i = 0;i < nbPlaques;i++) fichier << best->nbImpression[i] << endl;
+        fichier << nbPlaques << std::endl;
+        for(int i = 0;i < nbPlaques;i++) fichier << best->nbImpression[i] << std::endl;
         for (int i = 0; i < best->agencement.size(); i++) {
             fichier << best->agencement[i];
-            if (i % (int)*nbEmplacement == ((int)*nbEmplacement - 1)) fichier << endl;
+            if (i % (int)*nbEmplacement == ((int)*nbEmplacement - 1)) fichier << std::endl;
             else fichier << ",";
         }
         fichier << best->coutTotal;
     }
     else
     {
-        cout << "ERREUR: Impossible d'ouvrir le fichier." << endl;
+        std::cout << "ERREUR: Impossible d'ouvrir le fichier." << std::endl;
     }
    
+}
+
+bool CheckValiditePlaque() {
+
+    return true;
+}
+
+bool CheckValiditeImpression() {
+
+    return true;
+}
+
+void CalculCout(Solution* current, float*nbEmplacement,float*coutFeuille,float*coutPlaque) {
+    
+    int nbImpression = 0;
+    int nbPlaques = current->agencement.size() % ((int)*nbEmplacement-1);
+    for (int i = 0; i < nbPlaques; i++) {
+        nbImpression += current->nbImpression[i];
+    }
+    current->coutTotal =  (nbImpression * *coutFeuille) + (nbPlaques * *coutPlaque) ;
+    std::cout << *coutFeuille << std::endl;
 }
 
 int main()
 {
 
-    vector<float> inputData;
+    std::vector<float> inputData;
     Solution best;
+    Solution current;
     int nbdataset;
-    cout << "Quel dataset a tester ? : ";
-    cin >> nbdataset;
+    std::cout << "Quel dataset a tester ? : ";
+    std::cin >> nbdataset;
     lecture(&inputData, &nbdataset);
-    /*
     best.agencement.push_back(0);
     best.agencement.push_back(1);
     best.agencement.push_back(2);
     best.agencement.push_back(2);
-    best.agencement.push_back(0);
-    best.agencement.push_back(1);
-    best.agencement.push_back(2);
-    best.agencement.push_back(2);
-    best.nbImpression.push_back(5000);
-    best.nbImpression.push_back(5500);
-    best.coutTotal = 4555;
-    */
+    best.nbImpression.push_back(2);
+
+    
+    CalculCout(&best,&inputData[1], &inputData[inputData.size()-2], &inputData[inputData.size() - 1]);
+
+
     ecriture(&inputData[1], &best, &nbdataset);
 }
 
