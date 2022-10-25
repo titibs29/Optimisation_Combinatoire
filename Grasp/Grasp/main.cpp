831c82af68ad87a6224cb5a81e666d83e5fd8143
Veysf <florent.veys@std.heh.be>
2022-10-25 18:38:41 +0200

    définition des fonctions au début

--- Grasp/Grasp/main.cpp
+++ Grasp/Grasp/main.cpp
@@ -16,7 +16,7 @@
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
 // parametres
-#define RUNTIME 60				// temps de la boucle (secondes)
+#define RUNTIME 10			// temps de la boucle (secondes)
 #define NBCANDIDATES 20			// nombre de candidats /!\ MUST BE HIGHER THAN NUMBER OF THREADS
 #define NBITERLOCAL 10000			// nombre d'iteration locale
 #define NBTHREADS 8 // nombre de threads, utilisé std::thread::hardware_concurrency() pour utilisé le nombre de threads sur la machine
@@ -47,10 +47,27 @@ struct Solution {
 	bool isUsedInThread = false;
 };
 
+// fonctions
+bool lecture(Entree* entree, std::string input);
+bool ecriture(Solution* solution, unsigned char* nbEmplacement, unsigned short int* nbDataset);
 
+void calculCout(std::vector<unsigned char>* agencement,
+	std::vector<unsigned int>* nbImpression,
+	unsigned short int* nbPlaques,
+	unsigned char* nbEmplacement,
+	float* coutTotal,
+	float* coutImpression,
+	float* coutFabrication);
+bool checkValiditePlaque(std::vector<unsigned char>* agencement, unsigned char* nbCouverture);
+void impressionParPlaque(std::vector<unsigned char>* agencement,
+	std::vector<unsigned int>* nbImpressions,
+	std::vector<unsigned int>* nbImpressionsParCouv,
+	unsigned short int* nbPlaques,
+	unsigned char* nbCouverture,
+	unsigned char* nbEmplacement);
 
 void init(Solution* current, unsigned char* nbEmplacements);
-void generationPlaques(Solution* current, std::vector<float> *poidsImpression, unsigned char* nbCouverture, unsigned char* nbEmplacement);
+void generationPlaques(Solution* current, std::vector<float>* poidsImpression, unsigned char* nbCouverture, unsigned char* nbEmplacement);
 void TableauPoids(std::vector<unsigned int>* nbImpression, std::vector<float>* poidsImpression);
 
 void SwitchAgencement(Solution* current);
