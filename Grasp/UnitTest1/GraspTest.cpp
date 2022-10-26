212aa42b54a23a0d1546869b33e60ea7c42b6e71
titibs29 <thibaut.rizzoli@hotmail.be>
2022-10-26 12:01:27 +0200

    ajout de tests suite à des bugs rencontrés à l'execution dans le code principal

--- Grasp/UnitTest1/GraspTest.cpp
+++ Grasp/UnitTest1/GraspTest.cpp
@@ -82,6 +82,7 @@ public:
 	}
 	};
 
+
 	TEST_CLASS(nbImpressions) {
 
 public:
@@ -588,6 +589,152 @@ public:
 
 
 	}
+
+	TEST_METHOD(Matrice4) {
+
+		std::vector<unsigned int> nbImpressionParCouv({ 4500,9000,16000 });
+		std::vector<unsigned char> agencement({ 0,2,0,0, 1,2,1,2, 1,0,2,1 });
+		unsigned char  nbCouverture = 3;
+		int nbPlaques = 3, nbEmplacement = 4;
+		std::vector<unsigned int> nbImpression(nbPlaques, 0);
+
+		char buffer[64];
+		int colPivot = 0, lignePivot = 0;
+		float lignePlusPetit = -FLT_MAX;
+		float pivot = 0;
+
+
+		std::vector<std::vector<float>> matrice(
+			(nbPlaques + 1),											// nombre de lignes
+			std::vector<float>((nbCouverture + nbPlaques + 1),			// nombre de colonnes
+				0.0f));
+
+
+		// remplissage matrice
+		for (unsigned short i = 0; i < nbPlaques; i++) {
+			for (unsigned short j = 0; j < nbEmplacement; j++) {
+				// indices de contraintes 
+				matrice.at(i).at(agencement.at((i * (nbEmplacement)) + j)) += 1.0;
+
+
+			}
+			// ligne resultat
+			matrice.at(i).at(nbCouverture + nbPlaques) = 1;
+		}
+
+		for (int i = 0; i < nbPlaques; i++) {
+			// matrice unitaire
+			matrice.at(i).at(nbCouverture + i) = 1;
+
+		}
+
+		for (int i = 0; i < nbCouverture; i++) {
+
+			// nombre d'impressions
+			matrice.at(nbPlaques).at(i) -= nbImpressionParCouv.at(i);
+		}
+
+		Logger::WriteMessage("matrice initiale: \n");
+		showLogger(&matrice);
+
+		while (!estOptimal(&matrice)) {
+			colPivot = 0;
+			lignePlusPetit = FLT_MAX;
+
+			// choix colonne pivot
+			for (int i = 0; i < matrice.at(0).size(); i++)
+			{
+
+				// choix de la colonne ayant la valeur la plus faible sous le 0
+				if (matrice.at(nbPlaques).at(i) < 0 &&
+					matrice.at(nbPlaques).at(i) < matrice.at(nbPlaques).at(colPivot)) {
+					colPivot = i;
+				}
+			}
+
+
+
+			// choix ligne pivot
+			for (int i = 0; i < matrice.size() - 1; i++) {
+				if (matrice.at(i).at(colPivot) != 0
+					&&
+					(matrice.at(i).at(nbCouverture + nbPlaques) / matrice.at(i).at(colPivot)) < lignePlusPetit) {
+
+					lignePivot = i;
+					lignePlusPetit = matrice.at(i).at(nbCouverture + nbPlaques) / matrice.at(i).at(colPivot);
+
+				}
+
+			}
+
+			snprintf(buffer, sizeof buffer, "coordonees pivot: %d %d \n", lignePivot, colPivot);
+			Logger::WriteMessage(buffer);
+
+			// pivotage
+
+
+			pivot = matrice.at(lignePivot).at(colPivot);
+
+
+
+
+			// calcul des nouvelles cases de la matrice
+			for (int ligne = 0; ligne < matrice.size(); ligne++) {
+				if (ligne != lignePivot) {
+
+					for (int col = 0; col < matrice.at(0).size(); col++) {
+
+						// case = case - (element ligne pivot * (element Colonne pivot / pivot))
+						if (
+							col != colPivot)
+						{
+
+							float step1 = matrice.at(ligne).at(colPivot) / pivot;
+
+							float step2 = matrice.at(lignePivot).at(col) * step1;
+
+							matrice.at(ligne).at(col) -= (step2);
+
+						}
+					}
+				}
+			}
+
+			// diviser chaque membre de la ligne du pivot par le pivot
+			for (int lig = 0; lig < matrice.size(); lig++) {
+
+				matrice.at(lig).at(colPivot) /= pivot;
+
+			}
+
+			// mise a zero de chaque membre de la colonne du pivot, sauf le pivot
+			for (int ligne = 0; ligne < matrice.size(); ligne++) {
+				if (ligne != lignePivot) {
+					matrice.at(ligne).at(colPivot) = 0;
+				}
+			}
+
+			showLogger(&matrice);
+
+		}
+
+		// on a la matrice optimale, on extrait les valeurs souhaitées
+		Logger::WriteMessage("matrice optimisee: \n");
+		showLogger(&matrice);
+		for (int i = 0; i < nbPlaques; i++) {
+			nbImpression.at(i) = ceil(matrice.at(nbPlaques).at(nbCouverture + i));
+
+			snprintf(buffer, sizeof buffer, "impressions de la plaque %d: %d \n", i, nbImpression.at(i));
+			Logger::WriteMessage(buffer);
+		}
+
+		snprintf(buffer, sizeof buffer, "impressions totales: %.f \n", matrice.at(nbPlaques).at(nbCouverture + nbPlaques));
+		Logger::WriteMessage(buffer);
+
+
+
+	}
+
 	};
 
 }
